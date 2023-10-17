b7b2ee75b39ef05b08b2b98d4ba771c973a82df2
abell370 <ituniverse.sandbox@gmail.com>
2023-10-17 14:46:41 +0200

    + clock basic metod

--- seance5_cas3/src/main/java/Clock.java
+++ seance5_cas3/src/main/java/Clock.java
@@ -1,2 +1,22 @@
 public class Clock {
+    private State state;
+    public Clock() {
+        this.state = new ClockOffState(this);
+    }
+
+    public void changeState(State state) {
+        this.state = state;
+    }
+    public void arm(String hour){
+
+    }
+    public void triggerAlarm() {
+
+    }
+    public void stopAlarm(){
+
+    }
+    public void disarm(){
+
+    }
 }
---
+++ seance5_cas3/src/main/java/ClockOffState.java
@@ -0,0 +1,21 @@
+public class ClockOffState extends State{
+
+    public ClockOffState(Clock clock) {
+        super(clock);
+    }
+
+    @Override
+    public String onOn() {
+        return null;
+    }
+
+    @Override
+    public String onOff() {
+        return null;
+    }
+
+    @Override
+    public String onRing() {
+        return null;
+    }
+}
